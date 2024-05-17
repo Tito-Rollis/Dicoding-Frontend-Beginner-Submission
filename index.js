b57d3edc0cfaec50eef320360cab8c666ce09f87
Tito-Rollis <ouvalres@gmail.com>
2024-05-17 13:09:37 +0700

    fixed bug on deleteHandler for unread books

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const readedBooksContainer = document.getElementById('readed-books-container');
 const unreadBooksContainer = document.getElementById('unread-books-container');
 const statusBuku = document.getElementById('read-checked');
 const searchBook = document.getElementById('search-book');
+const unreadBooksTemplate = document.querySelector('[unread-books-template]');
 
 let penulisBukuInput = '';
 let judulBukuInput = '';
@@ -21,36 +22,34 @@ let existingArray = getLocalStorage ? JSON.parse(getLocalStorage) : [];
 
 // Functions
 const getUnreadBooks = () => {
-    let newDiv = '<h1 class="text-2xl font-semibold">Belum Selesai Dibaca</h1> ';
     const getIncompletedBooks =
         existingArray.length === 0 ? [] : existingArray.filter((book) => book.isComplete === false);
 
+    // RECATCH the unread Books if getIncompleteBooks have 1 or more books
+    if (getIncompletedBooks.length >= 1) {
+        unreadBooksContainer.textContent = '';
+    }
+
+    // Adding all unread books to unreadBooksContainer as Child Element
     getIncompletedBooks.forEach((book) => {
-        newDiv += `
-        <div id="${book.id}" class="box border border-purple-400 rounded-lg p-4 mt-3 gap-y-1">
-    <h1 class="text-xl font-medium">${book.title}</h1>
-                        <p>Penulis: ${book.author}</p>
-                        <p>Tahun Buku: ${book.year}</p>
+        const bookContainer = unreadBooksTemplate.content.cloneNode(true).children[0];
+        let title = bookContainer.querySelector('[unread-title]');
+        let author = bookContainer.querySelector('[unread-author]');
+        let tahunBuku = bookContainer.querySelector('[unread-tahun]');
+        let deleteBtn = bookContainer.querySelector('[unread-delete-btn]');
 
-                        <!-- Info -->
-                        <div class="flex items-center justify-between w-full">
-                            <!-- Chips -->
-                            <div
-                                class="box whitespace-nowrap justify-center rounded-lg text-sm h-8 px-4 w-min text-center leading-5 bg-yellow-400 text-yellow-700 font-medium"
-                            >
-                                <p>Belum dibaca</p>
-                            </div>
+        title.textContent = book.title;
+        author.textContent = book.author;
+        tahunBuku.textContent = `Tahun Buku: ${book.year}`;
 
-                            <div onclick="deleteHandler(${getIncompletedBooks.findIndex((e) => e.id === book.id)})"
-                                class=" h-8 px-4 text-center font-medium flex items-center cursor-pointer text-red-700 border border-red-700 rounded-lg"
-                            >
-                                <p>Hapus Buku</p>
-                            </div>
-                        </div>
-                        </div>
-    `;
+        bookContainer.setAttribute('id', `${book.id}`);
+
+        deleteBtn.addEventListener('click', () =>
+            deleteHandler(getIncompletedBooks.findIndex((el) => el.id === book.id))
+        );
+
+        return unreadBooksContainer.append(bookContainer);
     });
-    return (unreadBooksContainer.innerHTML = newDiv);
 };
 
 const getReadBooks = () => {
@@ -60,7 +59,7 @@ const getReadBooks = () => {
 
     getCompletedBooks.forEach((book) => {
         newDiv += `
-        <div id="${book.id}" class="box border border-purple-400 rounded-lg p-4 mt-3 gap-y-1">
+        <div id="${book.id}" class="box book border border-purple-400 rounded-lg p-4 mt-3 gap-y-1">
         <h1 class="text-xl font-medium">${book.title}</h1>
         <p>Penulis: ${book.author}</p>
         <p>Tahun Buku: ${book.year}</p>
@@ -69,7 +68,7 @@ const getReadBooks = () => {
         <div class="flex items-center justify-between w-full">
             <!-- Chips -->
             <div
-                class="box whitespace-nowrap justify-center rounded-lg text-sm h-8 px-4 w-min text-center leading-5 bg-green-400 text-green-700 font-medium"
+                class="box whiunreadBooksTemplatepace-nowrap justify-center rounded-lg text-sm h-8 px-4 w-max text-center leading-5 bg-green-400 text-green-700 font-medium"
             >
                 <p>Sudah dibaca</p>
             </div>
@@ -128,8 +127,12 @@ const searchBookHandler = (e) => {
 judulBuku.addEventListener('input', (e) => (judulBukuInput = e.target.value));
 penulisBuku.addEventListener('input', (e) => (penulisBukuInput = e.target.value));
 tahunBuku.addEventListener('input', (e) => (tahunBukuInput = e.target.value));
-statusBuku.addEventListener('input', (e) => (statusBukuInput = e.target.value));
+statusBuku.addEventListener('input', (e) => {
+    statusBukuInput = e.target;
+    console.log(statusBukuInput);
+});
 completeBtn.addEventListener('click', (e) => {
+    console.log(statusBukuInput);
     // Set id manually for each new book
     const id = existingArray.length === 0 ? 0 : existingArray[existingArray.length - 1].id + 1;
 
@@ -145,5 +148,4 @@ completeBtn.addEventListener('click', (e) => {
     localStorage.setItem('Books', JSON.stringify(existingArray));
     return getAllBooks();
 });
-
 searchBook.addEventListener('input', (e) => searchBookHandler(e));
